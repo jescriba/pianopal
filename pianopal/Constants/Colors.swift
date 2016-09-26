2a58b842ba36221ab1eb27a68677f51bad962cbc
jescriba <escribirajoshua@gmail.com>
2016-09-25 20:40:21 -0700

    Changes background color

--- pianopal/Constants/Colors.swift
+++ pianopal/Constants/Colors.swift
@@ -15,7 +15,7 @@ class Colors {
     static let toolBarBackground = UIColor(red: 0.94, green: 0.86, blue: 1, alpha: 1)
     static let toolbarActionText = UIColor(red: 0.94, green: 0.86, blue: 1, alpha: 1)
     static let changeToolbarActionText = UIColor(red: 1, green: 0.3, blue: 0.3, alpha: 1)
-    static let pickerBackground = UIColor(red: 1, green: 0.92, blue: 0.81, alpha: 1)
+    static let pickerBackground = UIColor.white
     static let highlightedWhiteKeyColor = UIColor(red: 0.0, green: 0.6, blue: 0.8, alpha: 1)
     static let highlightedBlackKeyColor = UIColor(red: 0.0, green: 0.5, blue: 0.75, alpha: 1)
     static let normalRightBarButtonColor = UIColor(red: 0.0, green: 0.6, blue: 0.8, alpha: 1)
@@ -26,7 +26,7 @@ class Colors {
     static let pressedPlayButtonColor = UIColor(red: 0.2, green: 0.5, blue: 0.98, alpha: 1)
     static let navigationTablViewCellSelectedBackgroundColor = UIColor(red: 1, green: 0.80, blue: 0.9, alpha: 1)
     static let chordTableSeparatorColor = UIColor(red: 0.9, green: 0.80, blue: 0.9, alpha: 1)
-    static let chordTableBackgroundColor = UIColor(red: 1, green: 0.92, blue: 0.81, alpha: 1)
+    static let chordTableBackgroundColor = UIColor.white
     static let tableViewCellSelectedColor = UIColor(red: 1, green: 0.82, blue: 0.81, alpha: 1)
     static let slideMenuBackgroundColor =  UIColor(red: 0.94, green: 0.84, blue: 0.97, alpha: 1)
     static let settingsSwitchTintColor = UIColor(red: 0.93, green: 0.55, blue: 0.72, alpha: 1)
